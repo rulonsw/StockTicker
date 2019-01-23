b3b58ff67eb3f5d90886ec828e8c532cedbaff57
Rulon Wood <rulonsdangerwoodiv@gmail.com>
2019-01-23 15:15:09 -0700

    Begin building sub-components for page view

---
+++ stock_ticker/src/components/StockEntryHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header-elements">
+    <header>Stock Watcher</header>
+    <input v-model="message" placeholder="Enter stock symbol...">
+    <button class="add-button">Yo</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "entryHeader",
+  components: {}
+};
+</script>
+
+<style lang="scss">
+	@import '../styles/header.scss'
+</style>
+
---
+++ stock_ticker/src/components/TickerContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ticker-container">
+    
+  </div>
+</template>
+
+<script>
+
+const TickerBodyView = {
+  props: {
+    msg: String
+  },
+
+};
+
+export {TickerBodyView};
+</script>
+
